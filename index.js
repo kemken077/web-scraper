389474e51f388b159de7599f3a035f3ddc7adf60
¨kemken077¨ <kemken077@gmail.com>
2023-02-26 16:13:19 -0300

    Scrap dolar info

--- index.js
+++ index.js
@@ -3,24 +3,37 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 const express = require('express');
 const app = express();
-const url = 'https://www.theguardian.com/international';
+const url = 'https://www.dolarhoy.com';
+
+const getCurrencyData = (currencyName, values) => {
+  const currency = {
+    currencyName,
+    values,
+  };
+  return currency;
+};
+
+const parseValue = (value, splitIdentifier) => {
+  return value !== '' ? parseFloat(value.split(splitIdentifier)[1]) : null;
+};
+
 
 axios(url)
   .then(response => {
     const html = response.data;
     const $ = cheerio.load(html);
-    const articles = [];
+    let currencyPrices = [];
 
-    $('.fc-item__title', html).each(function() {
-      const title = $(this).text();
-      const url = $(this).find('a').attr('href');
-      articles.push({
-        title,
-        url,
-      })
+    const tilesDolar = $('.tile.is-parent.is-7.is-vertical .tile.is-child', html);
+    tilesDolar.each(function() {
+      const title = $(this).find('.title').text();
+      const valueBuy = parseValue($(this).find('.values .compra').text(), '$');
+      const valueSale = parseValue($(this).find('.values .venta').text(), '$');
+      const currencyData = getCurrencyData(title, { buy: valueBuy, sale: valueSale});
+      currencyPrices.push(currencyData);
+
+      console.log(currencyData);
     });
-
-    console.log(articles);
 
   })
   .catch(err => console.log(err));
